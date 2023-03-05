570415b91b4e4a8490bbce2f616ab5e749622b15
Tommy-dev-cyber <tkim@helb-prigogine.be>
2023-03-05 16:13:38 +0100

    Ok
    Normalement c clean

---
+++ Assets/Scripts/Scriptable Objects/GunData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Weapon/Gun")]
+public class GunData : ScriptableObject
+{
+
+    [Header("Info")]
+    public new string name;
+
+    [Header("Shooting")]
+    public float Damage;
+    public float Max_Distance;
+
+    [Header("Reloading")]
+    public int Current_Ammo;
+    public int Magazine_Size;
+    [Tooltip("In RPM")] public float Fire_Rate;
+    public float Reload_Time;
+    [HideInInspector] public bool Reloading;
+
+}
